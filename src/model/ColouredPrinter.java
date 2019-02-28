8861afd72e9bbd52e113f1165c068ab0b2164f3e
muzrea <muzrea@hotmail.com>
2019-02-28 23:09:26 +0800

    create ColouredPrinter class

---
+++ src/model/ColouredPrinter.java
@@ -0,0 +1,9 @@
+package model;
+import model.Printer;
+
+public class ColouredPrinter extends Printer{
+    @Override
+    public void print(){
+        System.out.println("print colourful paper.");
+    }
+}
